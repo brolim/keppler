6e95148042cc93498aedc7f97b4345cda5b7e32d
Bruno Rolim <brolim@gmail.com>
2012-03-10 16:24:01 -0300

    mais uma tentativa pro css funcionar

--- config/application.rb
+++ config/application.rb
@@ -51,6 +51,6 @@ module Keppler
     config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
   end
 end
