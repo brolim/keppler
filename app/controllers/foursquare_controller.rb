0ffafba7d9454d18c43ee16670b2c2e3577013fe
Bruno Rolim <brolim@gmail.com>
2012-03-11 10:44:07 -0300

    faltou o render da action

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -13,6 +13,7 @@ class FoursquareController < ApplicationController
     req = Net::HTTP::Get.new(url.path)
 
     res = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
+    render :json=>{'status'=>'ok'}
   end
 
   def access_token
